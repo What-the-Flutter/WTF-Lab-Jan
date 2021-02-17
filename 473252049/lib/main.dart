41c030ee62a986320a0a68e6c3f86135f77cc54c
ilyin0 <ilyindeveloper@gmail.com>
2021-02-17 15:16:52 +0300

    Add Questionnaire Bot button to Home page

--- 473252049/lib/main.dart
+++ 473252049/lib/main.dart
@@ -153,9 +153,37 @@ class CategoriesPageContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
-      itemCount: _categories.length,
+      itemCount: _categories.length + 1,
       separatorBuilder: (context, index) => Divider(),
-      itemBuilder: (context, index) => CategoryView(_categories[index]),
+      itemBuilder: (context, index) => index == 0
+          ? _questionnaireBot(context)
+          : CategoryView(_categories[index - 1]),
     );
   }
+}
+
+Widget _questionnaireBot(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.only(left: 16, right: 16, top: 8),
+    child: TextButton(
+      style: Theme.of(context).textButtonTheme.style,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8),
+            child: Icon(
+              Icons.chat,
+              size: 28,
+            ),
+          ),
+          Text(
+            'Questionnaire Bot',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ],
+      ),
+      onPressed: () {},
+    ),
+  );
 }
