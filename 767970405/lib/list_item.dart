95464cc2eb6eff82005a594417aa7d5228d7930a
luffan <mihalkovgleb@gmail.com>
2021-03-02 08:37:54 +0300

    remove files

--- 767970405/lib/list_item.dart
+++
@@ -1,9 +0,0 @@
-class ListItem<T> {
-  T message;
-  bool isSelected;
-  bool isFeatured;
-  bool isImage;
-
-  ListItem(this.message,
-      {this.isSelected = false, this.isFeatured = false, this.isImage = false});
-}
