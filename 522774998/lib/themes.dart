5d59be1ebe8c5b9033422ce3b4f98bb76994ca85
Ann <nuta1312v@gmail.com>
2021-03-02 22:23:38 +0300

    feat:themes, navigation and ability to add a dialog

---
+++ 522774998/lib/themes.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+final darkTheme = ThemeData(
+  primaryColor: Colors.black,
+  accentColor: Colors.orange,
+  disabledColor: Colors.white,
+  backgroundColor: Colors.grey,
+  fontFamily: 'Quicksand',
+);
+
+final lightTheme = ThemeData(
+  primaryColor: Colors.deepPurple,
+  accentColor: Colors.orange,
+  disabledColor: Colors.deepPurple,
+  backgroundColor: Colors.white,
+  fontFamily: 'Quicksand',
+);
