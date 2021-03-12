5d5fa2751e6e377510e8f56dadc30c645d6e7df0
Denis Haritonovich <denisharitonovich17@gmail.com>
2021-03-12 13:19:24 +0300

    event page

--- 619740324/lib/home_page.dart
+++ 619740324/lib/home_page.dart
@@ -1,95 +1,147 @@
 import 'package:flutter/material.dart';
 
+import 'event_page.dart';
+import 'note.dart';
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+
+  List<Note> noteList = [
+    Note(
+      'Sport',
+      CircleAvatar(
+        child: Icon(
+          Icons.sports_soccer,
+        ),
+      ),
+      '',
+    ),
+    Note(
+      'FastFood',
+      CircleAvatar(
+        child: Icon(
+          Icons.fastfood,
+        ),
+      ),
+      '',
+    ),
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton (
-        child: Icon( Icons.add),
-        onPressed: () {},
-      ),
-      appBar: AppBar(
-        title: Center(
-          child: Text('Home'),
+        floatingActionButton: _floatingActionButton,
+        appBar: _appBar,
+        drawer: _drawer,
+        bottomNavigationBar: _bottomNavigationBar,
+        body: _homePageBody(),
+    );
+  }
+
+  ListView _homePageBody() {
+    return ListView.builder(
+      itemCount: noteList.length,
+      scrollDirection: Axis.vertical,
+      itemBuilder: (context, index) => ListTile(
+        title: Text(noteList[index].eventName),
+        leading: IconButton(
+          icon: noteList[index].iconData,
+          iconSize: 50,
+          onPressed: () {},
         ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.invert_colors),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(color: Colors.blue),
-              child: Text(
-                'Information',
-                style: TextStyle(
-                  fontSize: 30,
-                ),
+        subtitle: Text(noteList[index].subTittleEvent),
+        onTap: () async {
+          await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => EventPage(
+                title: noteList[index].eventName,
+                note: noteList[index],
               ),
             ),
-            ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text('Profile'),
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text(
-                'Settings',
-              ),
-            ),
-          ],
+          );
+        },
+      ),
+    );
+  }
+
+
+  BottomNavigationBar get _bottomNavigationBar {
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_view_day_sharp),
-            label: 'Daily',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.timeline),
-            label: 'TimeLine',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.explore),
-            label: 'Explore',
-          ),
-        ],
-        unselectedItemColor: Colors.grey,
-        selectedItemColor: Colors.blue,
-        showUnselectedLabels: true,
-      ),
-      body: ListView(
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_view_day_sharp),
+          label: 'Daily',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.timeline),
+          label: 'TimeLine',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.explore),
+          label: 'Explore',
+        ),
+      ],
+      unselectedItemColor: Colors.grey,
+      selectedItemColor: Colors.deepPurple,
+      showUnselectedLabels: true,
+    );
+  }
+
+  Drawer get _drawer {
+    return Drawer(
+      child: ListView(
         children: [
-          ListTile(
-            leading: Icon(Icons.fastfood),
-            title: Text(
-              'Food',
+          DrawerHeader(
+            decoration: BoxDecoration(color: Colors.deepPurple),
+            child: Text(
+              'Information',
+              style: TextStyle(
+                fontSize: 30,
+              ),
             ),
-            onTap: () {},
           ),
           ListTile(
-            leading: Icon(Icons.sports_soccer),
+            leading: Icon(Icons.account_circle),
+            title: Text('Profile'),
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
             title: Text(
-              'Sport',
+              'Settings',
             ),
-            onTap: () {},
           ),
         ],
       ),
+    );
+  }
+
+  AppBar get _appBar {
+    return AppBar(
+      title: Center(
+        child: Text('Home'),
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.invert_colors),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+
+  FloatingActionButton get _floatingActionButton {
+    return FloatingActionButton(child: Icon(Icons.add),
+      onPressed: () {},
     );
   }
 }
