c37adf072ce3c451335d37d3def7d5563087bf75
ilyin0 <ilyindeveloper@gmail.com>
2021-03-20 10:38:05 +0300

    Delete unnecessary ThemingApp widget

--- 473252049/lib/main.dart
+++ 473252049/lib/main.dart
@@ -1,10 +1,10 @@
-import 'package:chat_journal/pages/cubits/settings/settings_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'pages/cubits/categories/categories_cubit.dart';
 import 'pages/main_page.dart';
+import 'pages/settings/cubit/settings_cubit.dart';
 import 'repositories/local_database/local_database_categories_repository.dart';
 
 class CubitsObserver extends BlocObserver {
@@ -18,30 +18,18 @@ class CubitsObserver extends BlocObserver {
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = CubitsObserver();
+  final preferences = await SharedPreferences.getInstance();
   runApp(
-    MyApp(
-      preferences: await SharedPreferences.getInstance(),
+    BlocProvider(
+      create: (context) => SettingsCubit(
+        preferences: preferences,
+      ),
+      child: MyApp(),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  final SharedPreferences preferences;
-
-  const MyApp({Key key, @required this.preferences}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => SettingsCubit(
-        preferences: preferences,
-      ),
-      child: ThemingApp(),
-    );
-  }
-}
-
-class ThemingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<SettingsCubit, SettingsState>(
