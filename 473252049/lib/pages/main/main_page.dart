197e4675de1902a81bb274bbb5991b78afea39bd
ilyin0 <ilyindeveloper@gmail.com>
2021-04-04 14:18:58 +0300

    Add Menu-Unselect-records icon animation on MainPage

--- 473252049/lib/pages/main/main_page.dart
+++ 473252049/lib/pages/main/main_page.dart
@@ -18,7 +18,7 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
   final _children = [
     HomeTab(),
     Placeholder(),
@@ -33,39 +33,86 @@ class _MainPageState extends State<MainPage> {
     'Explore',
   ];
 
+  AnimationController menuUnselectController;
+
   int currentPageIndex = 0;
 
   void _onTabTap(int index) {
     setState(() {
       currentPageIndex = index;
     });
+    context.read<RecordsCubit>().unselectAll();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    menuUnselectController = AnimationController(
+      vsync: this,
+      duration: Duration(
+        milliseconds: 300,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    menuUnselectController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<RecordsCubit, RecordsState>(
       builder: (context, state) {
+        bool isSelectedRecord() {
+          if (state is RecordsLoadInProcess) return false;
+          return state.records.map((e) => e.record.isSelected).contains(true) &&
+              !(state is RecordsLoadInProcess);
+        }
+
+        if (isSelectedRecord() && currentPageIndex == 2) {
+          menuUnselectController.forward();
+        } else {
+          menuUnselectController.reverse();
+        }
+
         return Scaffold(
-          appBar: !(state is RecordsLoadInProcess) &&
-                  currentPageIndex == 2 &&
-                  state.records.map((e) => e.record.isSelected).contains(true)
-              ? AppBar(
-                  title: Text('Select'),
-                  leading: IconButton(
-                    icon: Icon(Icons.close),
-                    onPressed: () {
-                      context.read<RecordsCubit>().unselectAll();
-                    },
+          appBar: AppBar(
+            title: Text(
+              isSelectedRecord() && currentPageIndex == 2
+                  ? 'Select'
+                  : _tabNames[currentPageIndex],
+            ),
+            leading: Builder(
+              builder: (context) {
+                return IconButton(
+                  icon: AnimatedIcon(
+                    icon: AnimatedIcons.menu_close,
+                    progress: menuUnselectController,
                   ),
-                  actions: [
+                  onPressed: () {
+                    menuUnselectController.status == AnimationStatus.completed
+                        ? context.read<RecordsCubit>().unselectAll()
+                        : Scaffold.of(context).openDrawer();
+                  },
+                );
+              },
+            ),
+            actions: [
+              if (currentPageIndex == 2 && isSelectedRecord())
+                Row(
+                  children: [
                     IconButton(
                       icon: Icon(Icons.bookmark_outlined),
                       onPressed: () async {
-                        await context.read<RecordsCubit>().changeFavorite(state
-                            .records
-                            .where((element) => element.record.isSelected)
-                            .map((e) => e.record)
-                            .toList());
+                        await context.read<RecordsCubit>().changeFavorite(
+                              state.records
+                                  .where((element) => element.record.isSelected)
+                                  .map((e) => e.record)
+                                  .toList(),
+                            );
                         await context.read<RecordsCubit>().unselectAll(
                             records:
                                 state.records.map((e) => e.record).toList());
@@ -98,43 +145,42 @@ class _MainPageState extends State<MainPage> {
                       onPressed: () {
                         showDeleteRecordsDialog(context);
                       },
-                    )
+                    ),
                   ],
-                )
-              : AppBar(
-                  title: Text(
-                    _tabNames[currentPageIndex],
-                  ),
-                  actions: [
-                    if (currentPageIndex == 2)
-                      ShowFavoriteRecordsIconButton(state: state),
-                    if (currentPageIndex == 2)
-                      IconButton(
-                        icon: Icon(Icons.search),
-                        onPressed: () {
-                          showSearch(
-                            context: context,
-                            delegate: SearchRecordPage(
-                              context: context,
-                              records: state.records,
-                              withCategories: true,
-                            ),
-                          );
-                        },
-                      ),
+                ),
+              if (!isSelectedRecord() && currentPageIndex == 2)
+                Row(
+                  children: [
+                    ShowFavoriteRecordsIconButton(state: state),
                     IconButton(
-                      icon: Icon(
-                        context.read<SettingsCubit>().state.themeMode ==
-                                ThemeMode.light
-                            ? Icons.bedtime_outlined
-                            : Icons.bedtime,
-                      ),
+                      icon: Icon(Icons.search),
                       onPressed: () {
-                        context.read<SettingsCubit>().switchThemeMode();
+                        showSearch(
+                          context: context,
+                          delegate: SearchRecordPage(
+                            context: context,
+                            records: state.records,
+                            withCategories: true,
+                          ),
+                        );
                       },
                     ),
                   ],
                 ),
+              if (!isSelectedRecord())
+                IconButton(
+                  icon: Icon(
+                    context.read<SettingsCubit>().state.themeMode ==
+                            ThemeMode.light
+                        ? Icons.bedtime_outlined
+                        : Icons.bedtime,
+                  ),
+                  onPressed: () {
+                    context.read<SettingsCubit>().switchThemeMode();
+                  },
+                ),
+            ],
+          ),
           body: _children[currentPageIndex],
           drawer: MainPageDrawer(),
           bottomNavigationBar:
