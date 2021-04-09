6ebb1f99fe90228bd830b65d55187a9c7cb92b21
ilyin0 <ilyindeveloper@gmail.com>
2021-04-09 21:39:15 +0300

    Add time records statistics

---
+++ 473252049/lib/pages/statistics/charts/time_statistics/time_statistics_chart.dart
@@ -0,0 +1,59 @@
+import 'package:charts_flutter/flutter.dart';
+import 'package:flutter/material.dart';
+
+import 'time_statistics_data.dart';
+
+class TimeStatisticsChart extends StatelessWidget {
+  final List<TimeStatisticsData> dataList;
+
+  const TimeStatisticsChart({Key key, this.dataList}) : super(key: key);
+
+  List<Series<TimeStatisticsData, DateTime>> _getSeriesData() {
+    return [
+      Series(
+        id: 'Time Statistics',
+        data: dataList,
+        domainFn: (data, _) => data.dateTime,
+        measureFn: (data, _) => data.recordsCount,
+      )
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TimeSeriesChart(
+        _getSeriesData(),
+        animate: true,
+        defaultRenderer: BarRendererConfig<DateTime>(),
+        behaviors: [
+          SelectNearest(),
+          DomainHighlighter(),
+        ],
+        domainAxis: DateTimeAxisSpec(
+          renderSpec: SmallTickRendererSpec(
+            labelStyle: TextStyleSpec(
+              color: ColorUtil.fromDartColor(
+                Theme.of(context).textTheme.bodyText2.color,
+              ),
+            ),
+            lineStyle: LineStyleSpec(
+              color: ColorUtil.fromDartColor(
+                Theme.of(context).textTheme.bodyText2.color,
+              ),
+            ),
+          ),
+        ),
+        primaryMeasureAxis: NumericAxisSpec(
+          renderSpec: SmallTickRendererSpec(
+            labelStyle: TextStyleSpec(
+              color: ColorUtil.fromDartColor(
+                Theme.of(context).textTheme.bodyText2.color,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
