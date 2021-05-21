fd301be57b807b2094d174bbfd1da949f126e7d2
krudenko <everalst317@gmail.com>
2021-05-21 16:19:27 +0300

    Implementation of task 8. Replace 2 classes with 1 function that build list of messages. Add communication with homepage and messages page. Add possibility of adding new events, icon for new events, ability to delete events. Corrected mistakes from previous lesson: replace icon when tap on it, color highlighting messages with a long press.

--- 396575457/lib/screens/home_screen/event_data.dart
+++ 396575457/lib/screens/home_screen/event_data.dart
@@ -1,10 +1,12 @@
+import '../create_event_screen/messages_store.dart';
+
 import 'event.dart';
 
 class EventData {
   final List<Event> _data = <Event>[
-    Event('FirstEvent'),
-    Event('SecondEvent'),
-    Event('ThirdEvent'),
+    Event(title: 'FirstEvent', messages: EventsStore()),
+    Event(title: 'SecondEvent', messages: EventsStore()),
+    Event(title: 'ThirdEvent', messages: EventsStore()),
   ];
 
   List<Event> get eventsList {
@@ -15,7 +17,26 @@ class EventData {
     return _data.length;
   }
 
+  bool putAlreadyCreateEvent(Event event) {
+    _data.add(event);
+    return true;
+  }
+
+  bool put(String title) {
+    _data.add(Event(title: title));
+    return true;
+  }
+
   String getTitleByIndex(int i) {
-    return _data.elementAt(i).title;
+    return _data.elementAt(i).titleString;
+  }
+
+  Event getEventByIndex(int i) {
+    return _data[i];
+  }
+
+  bool removeEventByIndex(int i) {
+    _data.removeAt(i);
+    return true;
   }
 }
