d138b5a548de7698a27c8215d71e2b3774f5e1ec
Andrei Khanetski <v.andreihaneckii@gmail.com>
2021-07-06 03:47:37 +0300

    add home page, drawer, bottom navigation

---
+++ 1050483244/lib/pages/timeline_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TimelinePage extends StatelessWidget {
+  const TimelinePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Timeline')),
+    );
+  }
+}
