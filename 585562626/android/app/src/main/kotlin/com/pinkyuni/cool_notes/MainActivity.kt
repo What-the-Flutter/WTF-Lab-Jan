db33b4c2cceb3c4c54b82689f2537418c48dd776
PinkyUni <anya.beliatskaya.751001@gmail.com>
2021-08-04 15:54:00 +0300

    finish first task

---
+++ 585562626/android/app/src/main/kotlin/com/pinkyuni/cool_notes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pinkyuni.cool_notes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 585562626/lib/category.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  final Color color;
+  final String name;
+  final String image;
+
+  Category(this.name, this.color, this.image);
+}
