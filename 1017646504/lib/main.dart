573b3a49b19a660c255aafeb929e68d6bb109311
илья авжи <poker.plut@mail.ru>
2021-08-18 15:47:45 +0300

    dark theme; create, delete, edit pages.

--- 1017646504/lib/main.dart
+++ 1017646504/lib/main.dart
@@ -8,74 +8,250 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 List<String> message = <String>['First node', 'Second node', 'Third node '];
+List<String> page = <String>['First page', 'Second page', 'Third page'];
 
-class MyApp extends StatelessWidget {
+class ColorThemeProvider extends StatefulWidget {
+  final GlobalKey<ColorThemeProviderState> providerKey;
+  final Widget child;
+
+  ColorThemeProvider({required this.providerKey, required this.child}) : super(key: providerKey);
+
+  @override
+  ColorThemeProviderState createState() => ColorThemeProviderState();
+}
+
+class ColorThemeProviderState extends State<ColorThemeProvider> {
+  bool _isDark = false;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter',
-      home: Scaffold(
-        body: ListView.separated(
-          itemBuilder: (context, index) {
-            return Padding(
-                padding: const EdgeInsets.all(8.0),
-                // child: Text(
-                //   'запись $index',
-                //   textScaleFactor: 2,
-                // ),
-                child: TextButton.icon(
-                    onPressed: () {
-                      _navigateToNextScreen(context);
-                    },
-                    icon: const Icon(Icons.text_fields),
-                    label: Text('событие $index')));
-          },
-          separatorBuilder: (_, __) => Container(
-            height: 1,
-            color: Colors.black,
-          ),
-          itemCount: 20,
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.business),
-              label: 'Business',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.car_rental),
-              label: 'Race',
-            ),
-          ],
-          currentIndex: 0,
-          selectedItemColor: Colors.amber[800],
-          onTap: (_) {},
-        ),
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          centerTitle: true,
-          title: const Text('Flutter'),
-          leading: IconButton(
-            icon: const Icon(Icons.wb_sunny),
-            onPressed: () {},
-          ),
-        ),
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: () {},
-          label: const Text('bolt'),
-          icon: const Icon(Icons.bolt),
-          backgroundColor: Colors.green,
-        ),
+    return _isDark ? _darkTheme : _lightTheme;
+  }
+
+  void switchTheme() => setState(() {
+        _isDark = !_isDark;
+      });
+
+  ColorTheme get _lightTheme => ColorTheme(
+        widget.providerKey,
+        child: widget.child,
+        primaryButtonColor: Colors.black,
+        primaryBackgroundColor: Colors.yellow,
+        primaryTileColor: Colors.yellow,
+        primaryTextColor: Colors.black,
+      );
+
+  ColorTheme get _darkTheme => ColorTheme(
+        widget.providerKey,
+        child: widget.child,
+        primaryButtonColor: Colors.white,
+        primaryTileColor: Colors.black,
+        primaryBackgroundColor: Colors.black,
+        primaryTextColor: Colors.white,
+      );
+}
+
+class ColorTheme extends InheritedWidget {
+  final GlobalKey<ColorThemeProviderState> _providerKey;
+  final Color primaryBackgroundColor;
+  final Color primaryButtonColor;
+  final Color primaryTextColor;
+  final Color primaryTileColor;
+  @override
+  final Widget child;
+
+  ColorTheme(
+    this._providerKey, {
+    required this.primaryButtonColor,
+    required this.primaryBackgroundColor,
+    required this.primaryTextColor,
+    required this.primaryTileColor,
+    required this.child,
+  }) : super(child: child);
+
+  @override
+  bool updateShouldNotify(ColorTheme oldWidget) {
+    return true;
+  }
+
+  void switchTheme() => _providerKey.currentState!.switchTheme();
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+void _navigateToNextScreen(BuildContext context) {
+  Navigator.of(context).push(
+    MaterialPageRoute(builder: (context) => NewScreen()),
+  );
+}
+
+class _MyAppState extends State<MyApp> {
+  final GlobalKey<ColorThemeProviderState> _key = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return ColorThemeProvider(
+      providerKey: _key,
+      child: const MaterialApp(
+        title: 'Flutter',
+        home: MainPage(),
       ),
     );
   }
 
   void _navigateToNextScreen(BuildContext context) {
     Navigator.of(context).push(MaterialPageRoute(builder: (context) => NewScreen()));
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  bool isOnCreate = false;
+  bool isOnEdit = false;
+  int index = -1;
+  final Set selected = <int>{};
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:
+          context.dependOnInheritedWidgetOfExactType<ColorTheme>()?.primaryBackgroundColor,
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.separated(
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(8),
+              itemCount: page.length,
+              separatorBuilder: (_, __) => Container(
+                height: 1,
+                color: Colors.black,
+              ),
+              itemBuilder: (context, index) {
+                return _PageTile(
+                  isSelected: selected.contains(index),
+                  key: UniqueKey(),
+                  onSelected: () {
+                    if (selected.contains(index)) {
+                      selected.remove(index);
+                    } else {
+                      selected.add(index);
+                    }
+                    setState(() {});
+                  },
+                  pageText: page[index],
+                  onEdit: () {
+                    isOnEdit = true;
+                    this.index = index;
+                    textController.text = page[index];
+                    setState(() {
+
+                    });
+                  },
+                );
+              },
+            ),
+          ),
+          if (isOnCreate || isOnEdit)
+            TextField(
+              controller: textController,
+              decoration: InputDecoration(
+                hintText: 'Enter a Page Name',
+                hintStyle: TextStyle(
+                  color: context.dependOnInheritedWidgetOfExactType<ColorTheme>()?.primaryTextColor,
+                ),
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    if (isOnEdit) {
+                      page[index] = textController.text;
+                      index = -1;
+                      isOnEdit = false;
+                    } else {
+                      page.add(textController.text);
+                    }
+                    textController.clear();
+                    isOnCreate = false;
+                    setState(() {});
+                  },
+                  icon: Icon(
+                    isOnEdit ? Icons.check : Icons.send,
+                    color: context
+                        .dependOnInheritedWidgetOfExactType<ColorTheme>()
+                        ?.primaryButtonColor,
+                  ),
+                ),
+              ),
+            )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.car_rental),
+            label: 'Race',
+          ),
+        ],
+        currentIndex: 0,
+        selectedItemColor: Colors.amber[800],
+        onTap: (_) {},
+      ),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                setState(
+                  () {
+                    isOnCreate = true;
+                  },
+                );
+              },
+              icon: const Icon(
+                Icons.add,
+              )),
+          if (selected.isNotEmpty)
+            IconButton(
+              onPressed: () {
+                for (var i in selected.toList()..sort((a, b) => b.compareTo(a))) {
+                  page.removeAt(i);
+                }
+                print(page);
+                selected.clear();
+                setState(() {});
+              },
+              icon: const Icon(
+                Icons.delete,
+                color: Colors.white,
+              ),
+            )
+        ],
+        backgroundColor: Colors.green,
+        centerTitle: true,
+        title: const Text('Flutter'),
+        leading: IconButton(
+          icon: const Icon(Icons.wb_sunny),
+          onPressed: () {
+            context.dependOnInheritedWidgetOfExactType<ColorTheme>()?.switchTheme();
+          },
+        ),
+      ),
+    );
   }
 }
 
@@ -89,25 +265,31 @@ class NewScreen extends StatefulWidget {
 class _NewScreenState extends State<NewScreen> {
   bool isOnEdit = false;
   int index = -1;
-  final selected = <int>{};
+  final Set selected = <int>{};
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor:
+          context.dependOnInheritedWidgetOfExactType<ColorTheme>()?.primaryBackgroundColor,
       appBar: AppBar(
         title: const Text('New Screen'),
         actions: [
           if (selected.isNotEmpty)
             IconButton(
-                onPressed: () {
-                  for (var i in selected.toList()..sort((a, b) => b.compareTo(a))) {
-                    message.removeAt(i);
-                  }
-                  print(message);
-                  selected.clear();
-                  setState(() {});
-                },
-                icon: const Icon(Icons.delete))
+              onPressed: () {
+                for (var i in selected.toList()..sort((a, b) => b.compareTo(a))) {
+                  message.removeAt(i);
+                }
+                print(message);
+                selected.clear();
+                setState(() {});
+              },
+              icon: Icon(
+                Icons.delete,
+                color: context.dependOnInheritedWidgetOfExactType<ColorTheme>()?.primaryButtonColor,
+              ),
+            )
         ],
       ),
       body: Column(
@@ -144,6 +326,9 @@ class _NewScreenState extends State<NewScreen> {
             controller: textController,
             decoration: InputDecoration(
               hintText: 'Enter a message',
+              hintStyle: TextStyle(
+                color: context.dependOnInheritedWidgetOfExactType<ColorTheme>()?.primaryTextColor,
+              ),
               suffixIcon: IconButton(
                 onPressed: () {
                   if (isOnEdit) {
@@ -156,11 +341,96 @@ class _NewScreenState extends State<NewScreen> {
                   textController.clear();
                   setState(() {});
                 },
-                icon: Icon(isOnEdit ? Icons.check : Icons.send),
+                icon: Icon(
+                  isOnEdit ? Icons.check : Icons.send,
+                  color:
+                      context.dependOnInheritedWidgetOfExactType<ColorTheme>()?.primaryButtonColor,
+                ),
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _PageTile extends StatefulWidget {
+  _PageTile({
+    Key? key,
+    required this.pageText,
+    required this.onEdit,
+    required this.onSelected,
+    required this.isSelected,
+  }) : super(key: key);
+
+  final String pageText;
+  final Function() onEdit;
+  final Function() onSelected;
+  final bool isSelected;
+
+  @override
+  __PageTileState createState() => __PageTileState(
+        pageText: pageText,
+        onEdit: onEdit,
+        onSelected: onSelected,
+        isSelected: isSelected,
+      );
+}
+
+class __PageTileState extends State<_PageTile> {
+  String pageText;
+  Function() onEdit;
+  Function() onSelected;
+  bool isSelected;
+
+  __PageTileState({
+    required this.pageText,
+    required this.onEdit,
+    required this.onSelected,
+    required this.isSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onLongPress: () {
+        onSelected();
+      },
+      child: Container(
+        color: isSelected
+            ? Colors.black.withOpacity(0.05)
+            : context.dependOnInheritedWidgetOfExactType<ColorTheme>()?.primaryTileColor,
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: Row(
+          children: [
+            Expanded(
+              child: Text(
+                pageText,
+                style: TextStyle(
+                  fontSize: 22,
+                  color: context.dependOnInheritedWidgetOfExactType<ColorTheme>()?.primaryTextColor,
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: onEdit,
+              icon: Icon(
+                Icons.edit,
+                color: context.dependOnInheritedWidgetOfExactType<ColorTheme>()?.primaryButtonColor,
+              ),
+            ),
+            IconButton(
+                onPressed: () {
+                  _navigateToNextScreen(context);
+                },
+                icon: Icon(
+                  Icons.arrow_forward_ios,
+                  color:
+                      context.dependOnInheritedWidgetOfExactType<ColorTheme>()?.primaryButtonColor,
+                ))
+          ],
+        ),
       ),
     );
   }
@@ -209,18 +479,37 @@ class __MessageTileState extends State<_MessageTile> {
         onSelected();
       },
       child: Container(
-        color: isSelected ? Colors.black.withOpacity(0.05) : Colors.white,
+        color: isSelected
+            ? Colors.black.withOpacity(0.05)
+            : context.dependOnInheritedWidgetOfExactType<ColorTheme>()?.primaryTileColor,
         padding: const EdgeInsets.symmetric(vertical: 10),
         child: Row(
           children: [
             Expanded(
               child: Text(
                 messageText,
-                style: const TextStyle(fontSize: 22),
+                style: TextStyle(
+                  fontSize: 22,
+                  color: context.dependOnInheritedWidgetOfExactType<ColorTheme>()?.primaryTextColor,
+                ),
               ),
             ),
-            IconButton(onPressed: onEdit, icon: const Icon(Icons.edit)),
-            IconButton(onPressed: () {Clipboard.setData(ClipboardData(text: messageText));}, icon: const Icon(Icons.copy))
+            IconButton(
+                onPressed: onEdit,
+                icon: Icon(
+                  Icons.edit,
+                  color:
+                      context.dependOnInheritedWidgetOfExactType<ColorTheme>()?.primaryButtonColor,
+                )),
+            IconButton(
+                onPressed: () {
+                  Clipboard.setData(ClipboardData(text: messageText));
+                },
+                icon: Icon(
+                  Icons.copy,
+                  color:
+                      context.dependOnInheritedWidgetOfExactType<ColorTheme>()?.primaryButtonColor,
+                ))
           ],
         ),
       ),
