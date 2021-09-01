819c3084c9592163ed65d2ff9e8cd912bbd115e4
Egor Korshun <korshun.dev@gmail.com>
2021-09-01 14:36:11 +0300

    add first task

---
+++ 608754191/android/app/src/main/kotlin/com/example/task_wtf/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.task_wtf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 608754191/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:task_wtf/pages/homepage.dart';
+
+void main() => runApp(MaterialApp(
+      home: ChatJournal(),
+    ));
