aeee426fcf9af809c6d44ebe505f8c46ce130558
aiserrock <aicserrock@gmail.com>
2021-10-25 23:44:36 +0300

    complited task 6 (creating home screen)

--- 392686006/my_chat_journal/test/widget_test.dart
+++ 392686006/my_chat_journal/test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:my_chat_journal/main.dart';
+import 'package:my_chat_journal/ui/widgets/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const App());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
