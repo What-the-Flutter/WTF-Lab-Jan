24e2ad7663b84d41e6be4d5a99991b57528e1562
Egor Korshun <korshun.dev@gmail.com>
2021-10-28 20:47:49 +0300

    change integration tests

--- 608754191/integration_test/app_test.dart
+++ 608754191/integration_test/app_test.dart
@@ -3,21 +3,28 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'package:task_wtf/main.dart' as app;
+import 'package:task_wtf/pages/add_page/add_page.dart';
 
 void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   group(
     'App Test',
     () {
-      IntegrationTestWidgetsFlutterBinding.ensureInitialized();
       testWidgets(
-        'fab app test',
+        'add category',
         (tester) async {
           app.main();
           await tester.pumpAndSettle();
           final addPageButton = find.byType(FloatingActionButton).first;
           await tester.tap(addPageButton);
           await tester.pumpAndSettle();
-          expect(addPageButton, findsOneWidget);
+          final addPage = find.byType(AddPage);
+          expect(addPage, findsOneWidget);
+          await tester.enterText(find.byType(TextField), 'ABC');
+          await tester.pumpAndSettle();
+          await tester.tap(find.byIcon(Icons.done));
+          await tester.pumpAndSettle();
+          expect(find.text('ABC'), findsOneWidget);
         },
       );
       testWidgets(
@@ -25,9 +32,9 @@ void main() {
         (tester) async {
           app.main();
           await tester.pumpAndSettle();
-          final text = find.text('Questionnaire Bot');
+          await tester.tap(find.text('Timeline'));
           await tester.pumpAndSettle();
-          expect(text, findsOneWidget);
+          expect(find.byIcon(Icons.bookmark_border_outlined), findsOneWidget);
         },
       );
     },
--- 608754191/test/test_driver/integration_test_driver.dart
+++ 608754191/test/test_driver/integration_test_driver.dart
@@ -1,1 +1,3 @@
+import 'package:integration_test/integration_test_driver.dart';
+
 void main() => integrationDriver();
