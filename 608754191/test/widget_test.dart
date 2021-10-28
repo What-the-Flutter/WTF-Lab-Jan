cb8d9ce14949f92f640932d77341f7950a18d95e
Egor Korshun <korshun.dev@gmail.com>
2021-10-28 20:53:00 +0300

    add widget tests and change home page widget from test

--- 608754191/test/widget_test.dart
+++ 608754191/test/widget_test.dart
@@ -7,24 +7,64 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:task_wtf/main.dart';
+import 'package:task_wtf/entity/category.dart';
+import 'package:task_wtf/pages/home_page/widgets/category_list_tile.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    // await tester.pumpWidget(MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('Category list tile with empty subtitle', (tester) async {
+    final category = Category(
+      iconIndex: 1,
+      title: 'A',
+      subTitleMessage: '',
+      categoryId: 33,
+    );
+    await tester.pumpWidget(
+      wrapWidgetWithApp(
+        CategoryListTile(category: category),
+      ),
+    );
+    await tester.pumpAndSettle();
+    final createFirstMessageText = find.text('No events. Click to create one.');
+    expect(createFirstMessageText, findsOneWidget);
   });
+  testWidgets('Category list tile with non-empty subtitle', (tester) async {
+    final category = Category(
+      iconIndex: 1,
+      title: 'A',
+      subTitleMessage: 'B',
+      categoryId: 33,
+    );
+    await tester.pumpWidget(
+      wrapWidgetWithApp(
+        CategoryListTile(category: category),
+      ),
+    );
+    await tester.pumpAndSettle();
+    final createFirstMessageText = find.text('B');
+    expect(createFirstMessageText, findsOneWidget);
+  });
+  testWidgets('Category list tile with non-empty title', (tester) async {
+    final category = Category(
+      iconIndex: 1,
+      title: 'A',
+      subTitleMessage: 'B',
+      categoryId: 33,
+    );
+    await tester.pumpWidget(
+      wrapWidgetWithApp(
+        CategoryListTile(category: category),
+      ),
+    );
+    await tester.pumpAndSettle();
+    final createFirstMessageText = find.text('A');
+    expect(createFirstMessageText, findsOneWidget);
+  });
+}
+
+Widget wrapWidgetWithApp(Widget widget) {
+  return MaterialApp(
+    home: Scaffold(
+      body: widget,
+    ),
+  );
 }
