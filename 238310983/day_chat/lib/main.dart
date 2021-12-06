7f0047510abe73c92e31b71e535a97eebd2bdb1f
MishaPadalka <58531288+MishaPadalka@users.noreply.github.com>
2021-12-06 16:29:08 +0200

    Adding chat page

--- 238310983/day_chat/lib/main.dart
+++ 238310983/day_chat/lib/main.dart
@@ -1,6 +1,7 @@
 
-import 'package:day_chat/pages/home.dart';
 import 'package:flutter/material.dart';
+
+import 'pages/home.dart';
 void main() => runApp(const MaterialApp(
   debugShowCheckedModeBanner: false,
   home: Home(),
