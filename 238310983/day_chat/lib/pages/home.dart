b324b76e04cbe1d6f5fff5cfe82296842c8cd0c9
MishaPadalka <58531288+MishaPadalka@users.noreply.github.com>
2021-12-20 12:51:47 +0200

    Adding theme

--- 238310983/day_chat/lib/pages/home.dart
+++ 238310983/day_chat/lib/pages/home.dart
@@ -1,8 +1,9 @@
 import 'dart:ui';
-
+import 'package:day_chat/provider/theme_provider.dart';
 import 'package:flutter/material.dart';
 
 import 'chat_page.dart';
+
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -20,129 +21,146 @@ class _HomeState extends State<Home> {
   void initState() {
     super.initState();
     chats.addAll(['Family', 'Sport', 'Money']);
+    currentTheme.addListener(() => setState(() {}));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent.withOpacity(0.3),
-        flexibleSpace: ClipRRect(
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.transparent.withOpacity(0.4),
-                gradient: const LinearGradient(colors: [Colors.red, Colors.blue]),
+    return MaterialApp(
+      theme: CustomTheme.lightTheme,
+      darkTheme: CustomTheme.darkTheme,
+      themeMode: currentTheme.currentTheme,
+      home: Scaffold(
+        appBar: appBar(),
+        extendBodyBehindAppBar: true,
+        body: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/1.jpg'),
+                fit: BoxFit.cover,
               ),
+            ),
+            child: listView()),
+        floatingActionButton: floatingActionButton(),
+      ),
+    );
+  }
+
+  FloatingActionButton floatingActionButton() {
+    return FloatingActionButton(
+      child: Container(
+        width: 60,
+        height: 60,
+        child: const Icon(Icons.add),
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: LinearGradient(colors: [Colors.red, Colors.blue]),
+        ),
+      ),
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return alertDialog();
+          },
+        );
+      },
+    );
+  }
+
+  AlertDialog alertDialog() {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      backgroundColor: Colors.blueGrey,
+      title: const Text(
+        'Add New Chat',
+        style: TextStyle(color: Colors.white),
+      ),
+      content: TextField(style: const TextStyle(color: Colors.white), onChanged: (value) => userToDO = value),
+      actions: [
+        ElevatedButton(
+          onPressed: () {
+            setState(() => chats.add(userToDO));
+            Navigator.of(context).pop();
+          },
+          child: const Text('Add'),
+        ),
+      ],
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      flexibleSpace: ClipRRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.transparent.withOpacity(0.4),
+              gradient: const LinearGradient(colors: [Colors.red, Colors.blue]),
             ),
           ),
         ),
-        title: const Text(
-          'Chat Journal',
-          style: TextStyle(color: Colors.white),
-        ),
-        centerTitle: true,
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          onPressed: () {},
-        ),
-        iconTheme: const IconThemeData(color: Colors.white),
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.invert_colors_sharp),
-          ),
-        ],
       ),
-      extendBodyBehindAppBar: true,
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/1.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: ListView.builder(
-          itemCount: chats.length,
-          itemBuilder: (context, index) {
-            return Dismissible(
-              key: Key(chats[index]),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(18),
-                  ),
-                  color: Colors.transparent.withOpacity(0.6),
-                  gradient: const LinearGradient(colors: [Colors.red, Colors.blue]),
-                ),
-                alignment: Alignment.center,
-                margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
-                child: ListTile(
-                  onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const MessagesScreen(),
-                    ),
-                  ),
-                  title: Text(
-                    (chats[index]),
-                    style: const TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-              onDismissed: (direction) => setState(
-                () => chats.removeAt(index),
-              ),
-            );
-          },
-        ),
+      title: const Text(
+        'Chat Journal',
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Container(
-          width: 60,
-          height: 60,
-          child: const Icon(Icons.add),
-          decoration: const BoxDecoration(
-            shape: BoxShape.circle,
-            gradient: LinearGradient(colors: [Colors.red, Colors.blue]),
-          ),
+      centerTitle: true,
+      leading: IconButton(
+        icon: const Icon(Icons.menu),
+        onPressed: () {},
+      ),
+      iconTheme: const IconThemeData(color: Colors.white),
+      actions: <Widget>[
+        IconButton(
+          onPressed: () => currentTheme.toggleTheme(),
+          icon: const Icon(Icons.invert_colors_sharp),
         ),
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                backgroundColor: Colors.blueGrey,
-                title: const Text(
-                  'Add New Chat',
-                  style: TextStyle(color: Colors.white),
-                ),
-                content: TextField(
-                  style: const TextStyle(color: Colors.white),
-                  onChanged: (value) {
-                    userToDO = value;
-                  },
-                ),
-                actions: [
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(
-                        () => chats.add(userToDO),
-                      );
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text('Add'),
-                  ),
+      ],
+    );
+  }
+
+  Widget listView() {
+    return ListView.builder(
+      itemCount: chats.length,
+      itemBuilder: (context, index) {
+        return Dismissible(
+          key: Key(chats[index]),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(
+                Radius.circular(18),
+              ),
+              color: Colors.transparent.withOpacity(0.6),
+              gradient: LinearGradient(
+                colors: [
+                  Theme.of(context).colorScheme.primary,
+                  Theme.of(context).colorScheme.secondary,
                 ],
-              );
-            },
-          );
-        },
-      ),
+              ),
+            ),
+            alignment: Alignment.center,
+            margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
+            child: ListTile(
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const MessagesScreen(),
+                ),
+              ),
+              title: Text(
+                (chats[index]),
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          onDismissed: (direction) => setState(
+            () => chats.removeAt(index),
+          ),
+        );
+      },
     );
   }
 }
