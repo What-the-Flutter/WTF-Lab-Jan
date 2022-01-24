910b597ff045e14bd977401ff69dd5e8d02a10d5
Maksimilian Yakovlev <maksimilianyak@gmail.com>
2022-01-24 21:31:44 +0400

    provided flutterfire to app

--- 479714125/chat_diary/lib/main.dart
+++ 479714125/chat_diary/lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'firebase_options.dart';
 import 'src/app.dart';
 import 'src/app_home_cubit/app_cubit_observer.dart';
 import 'src/theme/cubit/cubit.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   BlocOverrides.runZoned(
     () async {
       final cubit = ThemeCubit();
--- 479714125/chat_diary/macos/Flutter/GeneratedPluginRegistrant.swift
+++ 479714125/chat_diary/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_auth
+import firebase_core
 import shared_preferences_macos
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
