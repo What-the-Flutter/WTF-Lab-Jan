cf52aa2286f8cbc789df9e38c11860cdd9af58ad
Maksimilian Yakovlev <maksimilianyak@gmail.com>
2022-01-25 12:49:38 +0400

    added dependency firebase_database

--- 479714125/chat_diary/macos/Flutter/GeneratedPluginRegistrant.swift
+++ 479714125/chat_diary/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,14 @@ import Foundation
 
 import firebase_auth
 import firebase_core
+import firebase_database
 import shared_preferences_macos
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
