22e1962296b75e78b55930ea4ac05998e08c56cb
Maksimilian Yakovlev <maksimilianyak@gmail.com>
2022-01-25 12:42:54 +0400

    added anon auth

--- 479714125/chat_diary/lib/main.dart
+++ 479714125/chat_diary/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -12,6 +15,13 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  final userCredential = await FirebaseAuth.instance.signInAnonymously();
+  final user = userCredential.user;
+  if (user == null) {
+    log('Anonymous auth failed');
+  } else {
+    log('Current user is $user');
+  }
   BlocOverrides.runZoned(
     () async {
       final cubit = ThemeCubit();
