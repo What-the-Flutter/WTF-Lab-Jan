391cdcf89827d2f3667e1d4b507edbb19260e15b
arthassson <garkun.business@gmail.com>
2022-01-26 16:10:33 +0300

    True Firebase Realtime Database

--- 599207837/lib/database/firebase_provider.dart
+++ 599207837/lib/database/firebase_provider.dart
@@ -1,73 +1,63 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 
 import '../entity/entities.dart';
 import '../main.dart';
 
 class FireBaseProvider {
-  static FirebaseFirestore fb = FirebaseFirestore.instance;
+  static FirebaseDatabase fb = FirebaseDatabase.instance;
 
   static Future<void> initFirebase() async {
     WidgetsFlutterBinding.ensureInitialized();
     await Firebase.initializeApp();
   }
 
-  static void newTopic(Topic newTopic) => fb.collection('Topics').add(newTopic.toJson());
+  static void newTopic(Topic newTopic) => fb.ref('topics/${newTopic.id}').set(newTopic.toJson());
 
   static void deleteTopic(Topic topic) {
     deleteAllMessages(topic);
-    fb.collection('Topics').doc(topic.nodeID).delete();
+    fb.ref('topics/${topic.id}').remove();
   }
 
-  static void updateTopic(Topic topic) =>
-      fb.collection('Topics').doc(topic.nodeID).update(topic.toJson());
+  static void updateTopic(Topic topic) => fb..ref('topics/${topic.id}').set(topic.toJson());
 
   static Future<Message> getLastMessage(Topic topic) async {
-    Message? ret;
-    await fb
-        .collection('Messages')
-        .where('uid', isEqualTo: userID)
-        .where('topic_id', isEqualTo: topic.id)
-        .orderBy('time_created')
-        .get()
-        .then((querySnapshot) {
-      ret = Message.fromJson(querySnapshot.docs.last.data(), topic,
-          nodeID: querySnapshot.docs.last.id);
-    });
-    return ret!;
+    Message ret;
+    final event = await fb
+        .ref('messages')
+        .orderByChild('uid')
+        .equalTo(userID)
+        .limitToFirst(1)
+        .once();
+    final data = (event.snapshot.value as List<Map<String, dynamic>>)[0];
+    ret = Message.fromJson(data, topic);
+    return ret;
   }
 
-  static Stream<QuerySnapshot<Map<String, dynamic>>> getTopics() =>
-      fb.collection('Topics').where('uid', isEqualTo: userID).snapshots();
+  static Stream<DataSnapshot> getTopics() =>
+      fb.ref('topics').orderByChild('uid').equalTo(userID).onValue.map((event) => event.snapshot);
 
-  static void newMessage(Message newMessage) => fb.collection('Messages').add(newMessage.toJson());
+  static void newMessage(Message newMessage) =>
+      fb.ref('messages/${newMessage.uuid}').set(newMessage.toJson());
 
-  static Stream<QuerySnapshot<Map<String, dynamic>>> getTopicMessages(Topic topic) => fb
-      .collection('Messages')
-      .where('uid', isEqualTo: userID)
-      .where('topic_id', isEqualTo: topic.id)
-      .orderBy('time_created', descending: true)
-      .snapshots();
+  static Stream<DataSnapshot> getTopicMessages(Topic topic) => fb
+      .ref('messages')
+      .orderByChild('uid')
+      .equalTo(userID)
+      .onValue
+      .map((event) => event.snapshot);
 
-  static Stream<QuerySnapshot<Map<String, dynamic>>> loadTypeFavourites(int typeID) => fb
-      .collection('Messages')
-      .where('uid', isEqualTo: userID)
-      .where('type_id', isEqualTo: typeID)
-      .snapshots();
+  static Stream<DataSnapshot> loadTypeFavourites(int typeID) => fb
+      .ref('messages')
+      .orderByChild('uid')
+      .equalTo(typeID)
+      .onValue
+      .map((event) => event.snapshot);
 
-  static void deleteMessage(Message o) => fb.collection('Messages').doc(o.nodeID).delete();
+  static void deleteMessage(Message o) => fb.ref('messages/${o.uuid}').remove();
 
-  static void updateMessage(Message o) =>
-      fb.collection('Messages').doc(o.nodeID).update(o.toJson());
+  static void updateMessage(Message o) => fb.ref('messages/${o.uuid}').set(o.toJson());
 
-  static void deleteAllMessages(Topic topic) async {
-    fb.collection('Messages').where('topic_id', isEqualTo: topic.id).get().then(
-      (querySnapshot) {
-        for (var doc in querySnapshot.docs) {
-          fb.collection('Messages').doc(doc.id).delete();
-        }
-      },
-    );
-  }
+  static void deleteAllMessages(Topic topic) async {}
 }
--- 599207837/lib/entity/entities.dart
+++ 599207837/lib/entity/entities.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:intl/intl.dart';
 
 import 'event.dart';
@@ -39,6 +41,39 @@ abstract class Message {
       default:
         return Note.fromJson(json, topic, nodeID: nodeID);
     }
+  }
+
+  static List<Map<String, dynamic>> parseData(String input) {
+    final exp = RegExp(r'\d+: {[^}]+}');
+    final expField = RegExp(r'[a-z_]+:');
+    final value = RegExp(r': [\w-:\s\.]+,');
+
+    final data = input.replaceAllMapped(expField, (match) {
+      final field = match.group(0);
+      return '"${field!.substring(0, field.length - 1)}":';
+    }).replaceAllMapped(value, (match) {
+      final temp = match.group(0);
+      return ': "${temp!.substring(2, temp.length - 1)}",';
+    }).replaceAllMapped(RegExp(r'"\d+"'), (match) {
+      final temp = match.group(0);
+      return '${temp!.substring(1, temp.length - 1)}';
+    });
+    print(data);
+
+    final list = _allStringMatches(data, exp).toList().map((note) => _toJson(note));
+
+    for (var x in list) {
+      print(x);
+    }
+    return list.map((e) => jsonDecode(e) as Map<String, dynamic>).toList();
+  }
+
+  static Iterable<String> _allStringMatches(String text, RegExp regExp) =>
+      regExp.allMatches(text).map((m) => m.group(0)!);
+
+  static String _toJson(String data) {
+    final clear = data.substring(data.indexOf('{', 1) + 1, data.length - 1);
+    return '{$clear}';
   }
 }
 
